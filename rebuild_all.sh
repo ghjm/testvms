62a494fa48e339afd70f29eebf59da77adadfe88
Graham Mainwaring <gmainwaring@ansible.com>
2016-07-21 15:02:35 -0400

    Moved build-snapshot-shutdown into site.yml playbook so it can all run under a single free/serial setting

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -3,5 +3,5 @@ read -p "Delete all test-* VMs and rebuild from scratch? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     rm -rf Virtual\ Machines/test-*
-    ./build_snapshot_shutdown.yml -l test-* --ask-vault-pass -e serial=4
+    ./site.yml -l test-* --ask-vault-pass -e serial=4 -e bssd=1
 fi
