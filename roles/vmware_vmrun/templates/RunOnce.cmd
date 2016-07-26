5a0681a9caf857e4e32b4014bac8aa20c6db7c6a
Graham Mainwaring <gmainwaring@ansible.com>
2016-07-26 17:12:53 -0400

    Added Chocolatey to Windows builds

--- roles/vmware_vmrun/templates/RunOnce.cmd
+++ roles/vmware_vmrun/templates/RunOnce.cmd
@@ -4,7 +4,19 @@ rem Configure Powershell Remoting
 
 powershell Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 powershell E:\ConfigureRemotingForAnsible.ps1
-powershell Set-ExecutionPolicy -ExecutionPolicy Default -Force
+
+rem Install 7zip
+start /wait E:\7z1602-x64.exe /S /D=="C:\Program Files\7-Zip"
+
+rem Install Chocolatey
+c:
+cd \Windows\Temp
+mkdir chocolatey
+cd chocolatey
+"C:\Program Files\7-Zip\7z" x E:\chocolatey.0.9.10.3.nupkg
+powershell tools\chocolateyInstall.ps1
+cd ..
+del /s /q chocolatey
 
 rem Install VMware Tools
 
